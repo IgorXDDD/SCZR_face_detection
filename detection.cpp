34d09e5af9c50eec7f60abacdfcf42609e1ec3d8
Igor <igori7@wp.pl>
2020-12-13 00:08:08 +0100

    functionized and added cascades

--- detection.cpp
+++ detection.cpp
@@ -3,23 +3,30 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <iostream>
-using namespace std;
-// using namespace cv;
-void detectAndDisplay( cv::Mat frame );
-cv::CascadeClassifier face_cascade;
-cv::CascadeClassifier eyes_cascade;
+// using namespace std;
+void detectFaces( cv::Mat frame, cv::CascadeClassifier face_cascade);
+
+void startCapturing();
+void create_video();
+
 
 int main( int argc, const char** argv )
 {
-    cv::String face_cascade_name = string("haarcascades/haarcascade_frontalface_alt.xml");
+    startCapturing();
+}
 
 
+
+void startCapturing()
+{
+    cv::String face_cascade_name = std::string("haarcascades/haarcascade_frontalface_alt.xml");
     //-- 1. Load the cascades
+    cv::CascadeClassifier face_cascade;
     if( !face_cascade.load( face_cascade_name ) )
     {
-        cout << "--(!)Error loading face cascade\n";
-        return -1;
+        std::cout << "--(!)Error loading face cascade\n";
     };
+
     int camera_device = 0;
     cv::VideoCapture capture;
 
@@ -27,70 +34,66 @@ int main( int argc, const char** argv )
     capture.open( camera_device );
     if ( ! capture.isOpened() )
     {
-        cout << "--(!)Error opening video capture\n";
-        return -1;
+        std::cout << "--(!)Error opening video capture\n";
     }
     cv::Mat frame;
     while ( capture.read(frame) )
     {
         if( frame.empty() )
         {
-            cout << "--(!) No captured frame -- Break!\n";
+            std::cout << "--(!) No captured frame -- Break!\n";
             break;
         }
         //-- 3. Apply the classifier to the frame
-        detectAndDisplay( frame );
+        detectFaces( frame, face_cascade);
         if( cv::waitKey(10) == 27 )
         {
             break; // escape
         }
     }
-    return 0;
 }
 
 
 
-void detectAndDisplay( cv::Mat frame )
+void detectFaces( cv::Mat frame, cv::CascadeClassifier face_cascade)
 {
     cv::Mat frame_gray;
     cv::Mat crop;
     cv::Mat res;
-    cv::Mat gray;
     int filenumber =1;
     cvtColor( frame, frame_gray, cv::COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
 
     //-- Detect faces
     std::vector<cv::Rect> faces;
-    std::vector<std::pair<std::string,cv::Mat>> faces_file;
+    // std::vector<std::pair<std::string,cv::Mat>> faces_file;
     face_cascade.detectMultiScale( frame_gray, faces );
 
-    cv::Rect roi_b, roi_c;
+    cv::Rect face_frame;
     
 
     //kazda z twarzy dodamy do wektora
     for ( size_t i = 0; i < faces.size(); i++ )
     {
 
-        roi_b.x = faces[i].x;
-        roi_b.y = faces[i].y;
-        roi_b.width = (faces[i].width);
-        roi_b.height = (faces[i].height);
+        face_frame.x = faces[i].x;
+        face_frame.y = faces[i].y;
+        face_frame.width = (faces[i].width);
+        face_frame.height = (faces[i].height);
 
-
-        crop = frame(roi_b);
+        crop = frame(face_frame);
         resize(crop, res, cv::Size(128, 128), 0, 0, cv::INTER_LINEAR); // This will be needed later while saving images
         // cvtColor(crop, gray, cv::COLOR_BGR2GRAY); // Convert cropped image to Grayscale
 
         // Form a filename
-        string filename = "";
-        stringstream ssfn;
-        ssfn << filenumber << ".jpg";
-        filename = ssfn.str();
-        filenumber++;
+        // string filename = "";
+        // stringstream ssfn;
+        // ssfn << filenumber << ".jpg";
+        // filename = ssfn.str();
+        // filenumber++;
 
-         //imwrite(filename, crop);
-         faces_file.push_back(std::make_pair(filename,crop));
+        // imwrite(filename, crop);
+        //  faces_file.push_back(std::make_pair(filename,crop));
 
         // to tylko do wyswietlania zielonego prostokata wokol twarzy
         cv::Point pt1(faces[i].x-2, faces[i].y-2); // Display detected faces on main window
@@ -98,11 +101,13 @@ void detectAndDisplay( cv::Mat frame )
         cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2, 8, 0); 
 
     }
+
     // zapisywanie do pliku kazdej z twarzy
     // for(auto i = faces_file.begin(); i!= faces_file.end(); i++)
     // {
     //     imwrite(i->first,i->second);
     // }
+
     //wyswietlanie
     imshow( "Capture - Face detection", frame );
 }
